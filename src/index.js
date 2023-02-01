c72b9db2225b748a0d765e531f72dc87ec0baa19
Christian Garcia <bones192nd@gmail.com>
2023-02-01 13:13:43 -0500

    requirements and 1 extra feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let formData = document.getElementById('new-task-description').value 
+    addToDo(formData)
+    form.reset
+  })
+  
 });
+
+
+function addToDo(todo){
+  
+  let newList = document.createElement('li')
+  let newBttn = document.createElement('button')
+
+  
+  newList.textContent =` ${todo} `
+  newBttn.textContent = "X"
+  
+  newBttn.addEventListener('click', deleteTodo)
+  newList.appendChild(newBttn)
+
+  document.getElementById('tasks').appendChild(newList)
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove()
+}
